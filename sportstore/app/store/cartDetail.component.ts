e63eda7b1e65619e9e2b77d6dfe703aae44f5cc1
Mikhail Grinshteyn <mgrinsht@gmail.com>
2017-04-19 22:54:45 -0400

    add details page

---
+++ sportstore/app/store/cartDetail.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Mikhail on 4/19/2017.
+ */
+import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: "cartDetail.component.html"
+})
+export class CartDetailComponent {
+
+  constructor(public cart: Cart) { }
+
+}
--- sportstore/app/store/store.module.ts
+++ sportstore/app/store/store.module.ts
@@ -4,14 +4,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
 
 import { ModelModule } from '../model/model.module';
 import { StoreComponent } from './store.component';
 import { CounterDirective } from './counter.directive';
+import { CartSummaryComponent } from "./cartsummary.component";
+import { CartDetailComponent } from "./cartDetail.component";
+import { CheckoutComponent } from "./checkout.component";
+
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent, CounterDirective],
-  exports: [StoreComponent]
+  imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
+  declarations: [StoreComponent, CounterDirective,
+    CartSummaryComponent, CartDetailComponent, CheckoutComponent,
+    HighlightDirective],
+  exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
 export class StoreModule { }
